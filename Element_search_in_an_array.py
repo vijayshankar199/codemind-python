d0a13fcc81cb12f2ca155f89c83fd4c5ed404b2d
vijayshankar199 <117157165+vijayshankar199@users.noreply.github.com>
2023-05-02 14:32:26 +0530

    n=int(input()) l=list(map(int,input().split())) e=int(input()) for i in range(len(l)):     if l[i]==e:         print(True)         break else:     print(False)

--- Element_search_in_an_array.py
+++ Element_search_in_an_array.py
@@ -1,11 +1,9 @@
-x=int(input())
+n=int(input())
 l=list(map(int,input().split()))
-z=int(input())
-s=0
-for i in l:
-    if i==z:
-        s=s+1
-if s==0:
-    print(False)
+e=int(input())
+for i in range(len(l)):
+    if l[i]==e:
+        print(True)
+        break
 else:
-    print(True)
+    print(False)
