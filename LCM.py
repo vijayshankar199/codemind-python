2e196bd0313eaf88697d1d7d4e7c74865254d24d
vijayshankar199 <117157165+vijayshankar199@users.noreply.github.com>
2023-07-07 09:33:45 +0530

    # Python Program to find the L.C.M. of two input number
    def compute_lcm(x, y):
    
       # choose the greater number
       if x > y:
           greater = x
       else:
           greater = y
    
       while(True):
           if((greater % x == 0) and (greater % y == 0)):
               lcm = greater
               break
           greater += 1
    
       return lcm
    
    # Python Program to find the L.C.M. of two input number
    
    def compute_lcm(x, y):
    
       # choose the greater number
       if x > y:
           greater = x
       else:
           greater = y
    
       while(True):
           if((greater % x == 0) and (greater % y == 0)):
               lcm = greater
               break
           greater += 1
    
       return lcm
    
    num1 ,num2=map(int,input().split())
    print( compute_lcm(num1, num2))

---
+++ LCM.py
@@ -0,0 +1,38 @@
+# Python Program to find the L.C.M. of two input number
+
+def compute_lcm(x, y):
+
+   # choose the greater number
+   if x > y:
+       greater = x
+   else:
+       greater = y
+
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+   return lcm
+
+# Python Program to find the L.C.M. of two input number
+
+def compute_lcm(x, y):
+
+   # choose the greater number
+   if x > y:
+       greater = x
+   else:
+       greater = y
+
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+   return lcm
+
+num1 ,num2=map(int,input().split())
+print( compute_lcm(num1, num2))
