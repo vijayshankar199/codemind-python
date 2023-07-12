355c999a302e95e5cdbc767e9aab271856fac3ef
vijayshankar199 <117157165+vijayshankar199@users.noreply.github.com>
2023-07-12 11:29:42 +0530

    a,b=map(int,input().split()) c=[] s=o=0 for i in range(a):     l=list(map(int,input().split()))     c.append(l) for i in range(a):     for j in range(b):         if c[i][j]%2==0:             s=s+c[i][j]         else:              o=o+c[i][j] print(s,o)

---
+++ Sum_of_even_and__odd_elements_in_a_matrix.py
@@ -0,0 +1,13 @@
+a,b=map(int,input().split())
+c=[]
+s=o=0
+for i in range(a):
+    l=list(map(int,input().split()))
+    c.append(l)
+for i in range(a):
+    for j in range(b):
+        if c[i][j]%2==0:
+            s=s+c[i][j]
+        else:
+             o=o+c[i][j]
+print(s,o)
