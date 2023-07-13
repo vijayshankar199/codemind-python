48ea65ad4973123ee3f422858a623d7d3b9d43fe
vijayshankar199 <117157165+vijayshankar199@users.noreply.github.com>
2023-07-13 11:17:47 +0530

    l=list(map(str,input().split())) f=l[0] d=l[len(l)-1] z=[] c=[] for i in range(len(f)):     z.append(ord(f[i])) for i in range(len(d)):     c.append(ord(d[i])) print(chr(min(z)),chr(max(c)))

--- small_and_large_elements.py
+++ small_and_large_elements.py
@@ -1,14 +1,10 @@
 l=list(map(str,input().split()))
-m=[]
-m.append(l[0])
-m.append(l[len(l)-1])
-f=m[0]
+f=l[0]
+d=l[len(l)-1]
 z=[]
 c=[]
-d=m[1]
 for i in range(len(f)):
     z.append(ord(f[i]))
 for i in range(len(d)):
     c.append(ord(d[i]))
-
 print(chr(min(z)),chr(max(c)))
